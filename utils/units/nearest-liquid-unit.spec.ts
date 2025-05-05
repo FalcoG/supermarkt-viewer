56d15ce381f35904123c80cfe8c52c9bdcd79e01
Falco Gerritsjans <alias.fcb319@falco.pro>
2025-05-05 23:59:17 +0200

    feat: create nearest liquid finder

---
+++ utils/units/nearest-liquid-unit.spec.ts
@@ -0,0 +1,39 @@
+import { assertEquals } from "jsr:@std/assert";
+import { nearestLiquidUnit } from "./nearest-liquid-unit.ts";
+
+Deno.test("nearestLiquidUnit", () => {
+	assertEquals(nearestLiquidUnit(1000, "ml"), {
+		unit: "l",
+		volume: 1,
+	});
+
+	assertEquals(nearestLiquidUnit(3000, "ml"), {
+		unit: "l",
+		volume: 3,
+	});
+
+	assertEquals(nearestLiquidUnit(0.33, "l"), {
+		unit: "dl",
+		volume: 3.3,
+	});
+
+	assertEquals(nearestLiquidUnit(0.33, "l", ["l", "ml"]), {
+		unit: "ml",
+		volume: 330,
+	});
+
+	assertEquals(nearestLiquidUnit(1100, "ml", ["l", "ml"]), {
+		unit: "l",
+		volume: 1.1,
+	});
+
+	assertEquals(nearestLiquidUnit(330, "cl"), {
+		unit: "l",
+		volume: 3.3,
+	});
+
+	// assertEquals(nearestLiquidUnit(30, "ml"), {
+	// 	unit: "l",
+	// 	volume: 1,
+	// });
+});
---
+++ utils/units/nearest-liquid-unit.ts
@@ -0,0 +1,57 @@
+type LiquidUnits = "l" | "dl" | "cl" | "ml";
+
+export function nearestLiquidUnit(
+	volume: number,
+	unit: string,
+	whitelist?: Array<LiquidUnits>,
+): {
+	unit: LiquidUnits;
+	volume: number;
+} {
+	// https://en.wikipedia.org/wiki/Litre#SI_prefixes_applied_to_the_litre
+
+	// factor array order impacts the first found factor
+	const factors: Array<{ unit: LiquidUnits; multiple: number }> = [
+		{ unit: "l", multiple: 1 },
+		{ unit: "dl", multiple: 1e-1 },
+		{ unit: "cl", multiple: 1e-2 },
+		{ unit: "ml", multiple: 1e-3 },
+	];
+
+	// todo: precision float fixer - aka take lowest/highest factor so there are no floats
+	const currentFactor = factors.find((factor) => factor.unit === unit);
+
+	if (!currentFactor) {
+		throw new Error(`The liquid "${unit}" is not implemented.`);
+	}
+
+	const baseline = volume * currentFactor.multiple;
+
+	console.log(
+		"baseline (liter)",
+		baseline,
+	);
+
+	const relevantFactor = factors.filter((factor) => {
+		if (whitelist) {
+			return whitelist.includes(
+				factor.unit,
+			);
+		} else {
+			return true;
+		}
+	}).find((factor) => {
+		const convertedVolume = baseline / factor.multiple;
+
+		if (convertedVolume >= 1) return true;
+	});
+
+	if (relevantFactor) {
+		return {
+			unit: relevantFactor.unit,
+			volume: baseline / relevantFactor.multiple,
+		};
+	} else {
+		throw new Error(`Unable to determine most relevant unit`);
+	}
+}
