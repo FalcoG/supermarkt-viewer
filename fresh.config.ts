51c84b768218384419a779befa61625e15e6bfe1
Falco Gerritsjans <alias.fcb319@falco.pro>
2025-05-05 22:50:48 +0200

    fix: migrate to tailwind v4

--- fresh.config.ts
+++ fresh.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "$fresh/server.ts";
-import tailwind from "$fresh/plugins/tailwind.ts";
+// import tailwind from "$fresh/plugins/tailwind.ts";
+import tailwind from "@pakornv/fresh-plugin-tailwindcss";
 
 export default defineConfig({
-  plugins: [tailwind()],
+	plugins: [tailwind()], // temporary patch until resolved https://github.com/denoland/fresh/issues/2819
 });
