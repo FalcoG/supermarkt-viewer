f650993a5e7288e53a7a5d5b9e2abaeb8faec8f9
Falco Gerritsjans <alias.fcb319@falco.pro>
2026-04-19 16:53:56 +0200

    feat: restore choices from storage

--- signals/userPreferences.ts
+++ signals/userPreferences.ts
@@ -1,8 +1,29 @@
-import { signal } from "@preact/signals";
+import { effect, signal } from "@preact/signals";
 import { getSupermarketCompanies } from "../utils/get-supermarket-data.ts";
+
+const storageKeySupermarkets = "userPreferences_supermarkets";
+
+function getSavedPreferences(key: string) {
+	const storageValues = localStorage.getItem(key);
+
+	try {
+		if (!storageValues) return null
+
+		return JSON.parse(storageValues)
+	} catch {
+		return null
+	}
+}
 
 const defaultValues = getSupermarketCompanies().map((supermarket) =>
 	supermarket.supermarket_id
 );
 
-export const userPreferenceSupermarkets = signal<Array<string>>(defaultValues);
+export const userPreferenceSupermarkets = signal<Array<string>>(
+	getSavedPreferences(storageKeySupermarkets) || defaultValues
+);
+
+effect(() => {
+	// triggered on every value change
+	localStorage.setItem(storageKeySupermarkets, JSON.stringify(userPreferenceSupermarkets.value))
+});
