1690ba43f34342029f5cfaec96d310450f03cea0
Falco Gerritsjans <alias.fcb319@falco.pro>
2026-04-27 16:10:36 +0200

    feat: simplify logo retrieval

--- islands/UserPreferences.tsx
+++ islands/UserPreferences.tsx
@@ -43,8 +43,9 @@ export function UserPreferences() {
 								}}
 							/>{" "}
 							<img
-								src={store.supermarket_logo}
+								src={`/supermarket-logos/${store.supermarket_id}.svg`}
 								width="32px"
+								height="32px"
 								class="inline"
 								alt={store.supermarket}
 							/>
